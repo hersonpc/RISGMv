1f1c9ae188577c2698fd3e45c79d37e1196984c2
Herson Melo <hersonpc@gmail.com>
2017-09-19 08:41:34 -0300

    Arquivos iniciais do projeto

---
+++ R/mv_exec.R
@@ -0,0 +1,11 @@
+#' Executar Query no banco de dados MV
+#'
+#' Exibe a mensagem de ola mundo
+#' @author Herson Melo <hersonpc@gmail.com>
+#' @param query Query SQL a ser executada
+#' @return Conexao ao banco
+#' @export
+mv_exec <- function(query) {
+  print("Hello, world!")
+  return ("Hello, world!")
+}
